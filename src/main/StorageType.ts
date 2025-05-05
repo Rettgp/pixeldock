df6b1c371362422176770137cf14f667e293ceec
Garrett Phelps <garrphelps@gmail.com>
2025-05-05 18:17:17 -0500

    Fix DB  storage for custom games

--- src/main/StorageType.ts
+++ src/main/StorageType.ts
@@ -1,3 +1,5 @@
+import { app } from 'electron';
+import path from 'path';
 import PouchDB from 'pouchdb';
 
 export type CustomGame = {
@@ -13,4 +15,6 @@ export type Response = {
     rev: string;
 };
 
-export const customGameDb = new PouchDB<CustomGame>('custom-games');
+export const customGameDb = new PouchDB<CustomGame>(
+    path.join(app.getPath('appData'), 'game-launch-db', 'custom-games'),
+);
